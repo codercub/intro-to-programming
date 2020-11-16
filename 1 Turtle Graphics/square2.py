5cd2117339b80f5ed33dc545dc45efaf2059d56c
Zolbayar Magsar <zolbayar.magsar@fulbrightmail.org>
2020-11-16 22:26:24 +0800

    Add files via upload

---
+++ 1 Turtle Graphics/square2.py
@@ -0,0 +1,22 @@
+# square2.py
+
+# Written by Zolbayar Magsar
+# Nov 16, 2020
+# Davtalt ashiglasan huvilbar
+
+# https://en.wikipedia.org/wiki/Turtle_graphics
+
+# Turtle nomiin sang ashiglay
+import turtle
+
+# Ted the turtle - manai gol dur
+ted = turtle.Pen()
+
+# Ted-g yast melhii helbertei bolgoy
+ted.shape("turtle")
+
+# Dogol morond orson code-g 4 udaa davtakh:
+for x in range(4):
+    # Bagtsalsan 2 mor code n 1 block bolj hamtdaa ajillana
+    ted.forward(100)
+    ted.left(90)
