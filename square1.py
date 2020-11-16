53c1d32f37dfd0e3ad8ebcc83e096736633e518e
Zolbayar Magsar <zolbayar.magsar@fulbrightmail.org>
2020-11-16 22:20:38 +0800

    Add files via upload

---
+++ square1.py
@@ -0,0 +1,40 @@
+# square1.py
+
+# Written by Zolbayar Magsar
+# Nov 16, 2020
+# Davtalt ashiglaagui huvilbar
+
+# https://en.wikipedia.org/wiki/Turtle_graphics
+
+# Turtle nomiin sang ashiglay
+import turtle
+
+# Ted the turtle - manai gol dur
+ted = turtle.Pen()
+
+# Ted-g yast melhii helbertei bolgoy
+ted.shape("turtle")
+
+# Uragshaa 100 pixel-eer yavaarai, Ted!
+ted.forward(100)
+
+# Zuun tiish 90 gradusaar ergeerei, Ted!
+ted.left(90)
+
+# Uragshaa 100 pixel-eer yavaarai, Ted!
+ted.forward(100)
+
+# Zuun tiish 90 gradusaar ergeerei, Ted!
+ted.left(90)
+
+# Uragshaa 100 pixel-eer yavaarai, Ted!
+ted.forward(100)
+
+# Zuun tiish 90 gradusaar ergeerei, Ted!
+ted.left(90)
+
+# Uragshaa 100 pixel-eer yavaarai, Ted!
+ted.forward(100)
+
+# Zuun tiish 90 gradusaar ergeerei, Ted!
+ted.left(90)
