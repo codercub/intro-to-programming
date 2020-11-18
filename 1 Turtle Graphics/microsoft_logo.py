884077565a42ffa464594e3b10269f7900ebfe72
Zolbayar Magsar <zolbayar.magsar@fulbrightmail.org>
2020-11-18 12:19:11 +0800

    Add files via upload
    Microsoft logo drawn using Turtle graphics in Python.

---
+++ 1 Turtle Graphics/microsoft_logo.py
@@ -0,0 +1,63 @@
+# microsoft_logo.py
+
+# Written by Zolbayar Magsar
+# Nov 17, 2020
+# Function ashiglaagui huvilbar
+
+import turtle
+
+t = turtle.Pen()
+t.speed(0)
+
+# Ulaan dorvoljin
+t.penup()
+t.setpos(-450, 5)
+t.pendown()
+t.color("orange red", "orange red")
+t.begin_fill()
+for x in range(4):
+    t.forward(100)
+    t.left(90)
+t.end_fill()
+        
+# Tsenkher dorvoljin
+t.penup()
+t.setpos(-450, -105)
+t.pendown()
+t.color("deep sky blue", "deep sky blue")
+t.begin_fill()
+for x in range(4):
+    t.forward(100)
+    t.left(90)
+t.end_fill()
+
+# Nogoon dorvoljin
+t.penup()
+t.setpos(-340, 5)
+t.pendown()
+t.color("yellow green", "yellow green")
+t.begin_fill()
+for x in range(4):
+    t.forward(100)
+    t.left(90)
+t.end_fill()
+
+# Shar dorvoljin
+t.penup()
+t.setpos(-340, -105)
+t.pendown()
+t.color("gold", "gold")
+t.begin_fill()
+for x in range(4):
+    t.forward(100)
+    t.left(90)
+t.end_fill()
+
+# Microsoft bichig
+t.penup()
+t.setpos(-200, -100)
+t.pendown()
+t.pencolor("dark grey")
+t.write("Microsoft", font = ("Segoe UI", 80, "normal"))
+    
+t.hideturtle()
