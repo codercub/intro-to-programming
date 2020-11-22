e0f7136079e9f230fb7530e3c8a73073bbd726ba
Zolbayar Magsar <zolbayar.magsar@fulbrightmail.org>
2020-11-23 06:56:39 +0800

    Update convex_polygon.py

--- 1 Turtle Graphics/convex_polygon.py
+++ 1 Turtle Graphics/convex_polygon.py
@@ -14,11 +14,11 @@ ted = turtle.Pen()
 ted.speed(0)
 
 # Daraah ajluudiig hiih:
-# 1. Davtaltiin too bolon ergeltiin gradusiin hoorondiin hamaarliig ajiglah
-# 2. Toirgiin radius 360 baidgiig sanah
-# 3. Dorvoljnii huvid 4 * 90 = 360, gurvaljnii huvid 3 * 120 = 360 baina
-# 4. Asuultiin temdguudiin orond tohiroh utguudiig ogch 5, 6, 8, 10 talt dursuud zurah
-# 5. Davtaltiig ihesgej, ergeltiin ontsog bolon taluudiig bagasgasnaar tsagirag zurah
+# 1. Davtaltiin too bolon ergeltiin gradusiin hoorondiin hamaarliig ajiglah, uund:
+# - Toirgiin radius 360 baidag
+# - Tiimees dorvoljnii huvid 4 * 90 = 360, gurvaljnii huvid 3 * 120 = 360 gesen zui togtoltoi baina
+# 2. Asuultiin temdguudiin orond tohiroh utguudiig ogch 5, 6, 8, 10 talt dursuud zurah
+# 3. Davtaltiig ihesgej, ergeltiin ontsog bolon taluudiig bagasgasnaar toirog zurah
 for x in range(?):
     # Gurvaljnii taliig 200 pixel hemjeetei zurna
     ted.forward(100)
