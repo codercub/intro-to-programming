99fca2c5f2e52aa670d45aa41c5d1d125c5c9d27
Zolbayar Magsar <zolbayar.magsar@fulbrightmail.org>
2020-11-23 15:40:39 +0800

    Create stickman.py

---
+++ 1 Turtle Graphics/stickman.py
@@ -0,0 +1,29 @@
+# stickman.py
+
+# Written by Zolbayar Magsar
+# Sep 19, 2020
+
+import turtle as t
+
+# head
+t.circle(50)
+
+# arms
+t.right(30)
+t.forward(100)
+t.backward(100)
+t.right(120)
+t.forward(100)
+t.backward(100)
+
+# body
+t.left(60)
+t.forward(150)
+
+# legs
+t.right(30)
+t.forward(100)
+t.backward(100)
+t.left(60)
+t.forward(100)
+t.backward(100)
