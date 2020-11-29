10a05ac349759e5de08be96e5b49b908fb290e21
Zolbayar Magsar <zolbayar.magsar@fulbrightmail.org>
2020-11-29 18:12:09 +0800

    Create about_me.py

---
+++ Lesson 2/about_me.py
@@ -0,0 +1,16 @@
+# about_me.py
+
+# Written by @zmagsar
+# Nov 29, 2020
+
+# Hereglegchiin burtgel
+name = input("Ovog, ner: ")
+age = input("Surguuli: ")
+grade = input("Angi: ")
+
+# Medeelliig haruulah
+print()
+print("Tanii oruulsan medeelel:")
+print("Ovog, ner:", name)
+print("Nas:", age)
+print("Angi:", grade)
