92942c6aabca3be0231d7c746971c79ebb763af5
Zolbayar Magsar <zolbayar.magsar@fulbrightmail.org>
2020-12-02 16:00:47 +0800

    Create octagonal_star.py

---
+++ Lesson 2/octagonal_star.py
@@ -0,0 +1,22 @@
+# octagonal_star.py
+
+# Written by @zmagsar
+# Dec 2, 2020
+
+import turtle as t
+
+t.pensize(3)
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "white"]
+
+for x in range(8):
+    t.color("black", colors[x])
+    t.begin_fill()
+    for y in range(2):
+        t.forward(100)
+        t.right(45)
+        t.forward(100)
+        t.right(135)
+    t.end_fill()
+    t.right(45)
+
+t.hideturtle()
