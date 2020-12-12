f9c74e63fad6472a6c6d56754f0e2c0827177e6c
Zolbayar Magsar <zolbayar.magsar@fulbrightmail.org>
2020-12-12 17:07:13 +0800

    Create colorful_star_spiral.py

---
+++ Lesson 3/colorful_star_spiral.py
@@ -0,0 +1,14 @@
+# colorful_star_spiral.py
+
+# Written by @zmagsar
+# Dec 5, 2020
+
+import turtle as t
+
+t.pensize(5)
+colors = ["red", "pink", "brown", "violet", "purple"]
+
+for x in range(100):
+    t.pencolor(colors[x % 5])
+    t.forward(x * 10)
+    t.right(144)
