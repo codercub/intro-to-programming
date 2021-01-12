c82387703d9a3f9461a0f2500418be2649ac5956
Zolbayar Magsar <zolbayar.magsar@fulbrightmail.org>
2021-01-12 18:38:55 +0800

    update

---
+++ Lesson 5/number_guessing_game.py
@@ -0,0 +1,30 @@
+# number_guessing_game.py
+
+# Written by @zmagsar
+# Jan 12, 2021
+
+import random
+
+# 1~10 toonuudaas duriin too songoh
+number = random.randint(1, 10)
+
+# Ehnii udaa taah
+guess = int(input("Enter number 1~10: "))
+attempt = 1
+
+# Hervee buruu taasan bol dahij 2 udaa taah bolomj
+while guess != number:
+    if guess > number:
+        print("Go lower!")
+    else:
+        print("Go higher!")
+    guess = int(input("Enter number 1~10: "))
+    attempt += 1
+    if attempt == 4:
+        print("Game over.")
+        break
+
+# Toog zov taasan tohioldold
+else:
+    print("You guessed it right,", number, "is the number!")
+    print("Number of attempts:", attempt)
